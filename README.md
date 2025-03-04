7c1963a39fe5f62a4ab6004e59eb9ea820cba25d
AlexAntonik <antonikavv@gmail.com>
2025-03-04 15:14:25 +0900

    initial

---
+++ README.md
@@ -0,0 +1,48 @@
+<div align="center">
+
+## NixOS Config
+
+**Inspiration for the config
+[here](https://gitlab.com/Zaney/zaneyos).**
+
+</div>
+
+### Install
+
+```
+nix-shell -p git curl
+```
+
+```
+sh <(curl -L https://raw.githubusercontent.com/AlexAntonik/config/refs/heads/master/install.sh)
+```
+
+## Aliases
+
+# Rebuld config
+
+rebuild switch --flake
+
+```
+fr
+``` 
+
+# Flake update
+
+nix flake update
+
+```
+fu
+```
+
+# Remove previous generations
+
+```
+ncg
+```
+
+# Use install script/backup current
+
+```
+zu
+```
---
+++ hosts/alex/variables.nix
@@ -0,0 +1,21 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Tyler Kelley";
+  gitEmail = "tylerzanekelley@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "microsoft-edge"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "ghostty"; # Set Default System Terminal
+  keyboardLayout = "us,ru";
+  consoleKeyMap = "us";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+}
