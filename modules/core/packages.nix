c1d525288bf2264785b7fc51113047e3aaa873dc
AlexAntonik <antonikavv@gmail.com>
2025-03-14 20:59:43 +0000

    1

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -41,6 +41,7 @@
     microsoft-edge # Web browser
     telegram-desktop # Telegram client
     protonvpn-gui # ProtonVPN client
+    tor-browser
 
     # Bluetooth
     bluez-alsa # Bluetooth ALSA support
