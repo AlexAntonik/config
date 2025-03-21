a260011bba7b3b37186a034550e5708fa816de71
AlexAntonik <antonikavv@gmail.com>
2025-03-21 14:17:10 +0000

    toggle touchpad button

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -191,7 +191,7 @@ in
         ",XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
         ",XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ",XF86TouchpadToggle , exec, toggle_touchbar"
+        ",XF86TouchpadToggle , exec, toggle_touchpad"
         ",XF86AudioPlay, exec, playerctl play-pause"
         ",XF86AudioPause, exec, playerctl play-pause"
         ",XF86AudioNext, exec, playerctl next"
