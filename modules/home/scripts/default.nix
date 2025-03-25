665d4ac35ebe00c43fec87c2b63180628e5ed92f
AlexAntonik <antonikavv@gmail.com>
2025-03-25 14:42:42 +0300

    fzf

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -6,7 +6,6 @@
 {
   home.packages = [
     (import ./emopicker9000.nix { inherit pkgs; })
-    (import ./keybinds.nix { inherit pkgs; })
     (import ./task-waybar.nix { inherit pkgs; })
     (import ./nvidia-offload.nix { inherit pkgs; })
     (import ./wallsetter.nix {
