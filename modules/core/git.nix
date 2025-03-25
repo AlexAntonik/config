6178cfe0c00a9eb850a695608a347135c4155775
AlexAntonik <antonikavv@gmail.com>
2025-03-25 19:04:47 +0300

    commit from root test

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -1,6 +1,15 @@
-{ ... }:
+{ host, ... }:
+let
+  inherit (import ../../hosts/${host}/variables.nix) gitUsername gitEmail;
+in
 {
   programs.git = {
     enable = true;
+    config = {
+      user = {
+        name = gitUsername;
+        email = gitEmail;
+      };
+    };
   };
 }
