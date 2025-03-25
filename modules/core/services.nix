e12c0b56d834f87813f07946fc76751a6a668410
AlexAntonik <antonikavv@gmail.com>
2025-03-25 14:42:46 +0300

    fzf

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -23,6 +23,22 @@ in
     flatpak.enable = true;
     blueman.enable = true;
 
+    journald = {
+      extraConfig = ''
+        SystemMaxUse=100M
+        SystemKeepFree=1G
+        SystemMaxFileSize=50M
+        RuntimeMaxUse=100M
+
+        Storage=persistent
+        Compress=yes
+        SyncIntervalSec=5m
+
+        MaxRetentionSec=1month
+        MaxFileSec=1week
+      '';
+    };
+
     xserver = {
       enable = false;
       xkb = {
