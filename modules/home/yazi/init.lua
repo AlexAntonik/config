554693ec28930037bd08ccaf35be555cc416cf3b
AlexAntonik <antonikavv@gmail.com>
2025-04-19 23:08:41 +0300

    disable yazi git plugin from strange THEME err

--- modules/home/yazi/init.lua
+++ modules/home/yazi/init.lua
@@ -1,5 +1,5 @@
 require("full-border"):setup()
-require("git"):setup() --off becouse of error
+-- require("git"):setup() --off becouse of error
 require("smart-enter"):setup {
 	open_multi = true,
 }
