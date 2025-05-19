6aaca37e0e6834d0b5c819fcffc3d75fa6a8de92
AlexAntonik <antonikavv@gmail.com>
2025-05-19 18:01:53 +0300

    25.05 and some tweaks to get it fully working

--- home/stylix.nix
+++ home/stylix.nix
@@ -9,6 +9,9 @@
     btop.enable = false;
     hyprland.enable = false;
     hyprlock.enable = false;
+    #Even i don't have mako installed this fixes stylix shit
+    mako.enable = false;
+    
     ghostty.enable = false;
   };
 }
