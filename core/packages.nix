600f8e3200f2609de5b941d011ac35ef63de4454
AlexAntonik <antonikavv@gmail.com>
2025-05-20 10:20:15 +0300

    small refactor

--- core/packages.nix
+++ core/packages.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  pkgs-unstable,
   ...
 }:
 {
@@ -28,91 +27,86 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages =
-    (with pkgs; [
+  environment.systemPackages = with pkgs; [
 
-      # Programming languages
-      go # Go programming language
-      nixd # Nix LSP
-      dart # Dart language
-      kotlin # Kotlin language
-      typescript # Bad language
-      zulu # Open JDK fast
+    # Programming languages
+    go # Go programming language
+    nixd # Nix LSP
+    dart # Dart language
+    kotlin # Kotlin language
+    typescript # Bad language
+    zulu # Open JDK fast
 
-      # Development tools
-      docker-compose # Allows controlling Docker from a single file
-      meson # Build system
-      gradle # Build system
-      ninja # Build system
-      nixfmt-rfc-style # Nix code formatter
-      pkg-config # Package configuration tool
+    # Development tools
+    docker-compose # Allows controlling Docker from a single file
+    meson # Build system
+    gradle # Build system
+    ninja # Build system
+    nixfmt-rfc-style # Nix code formatter
+    pkg-config # Package configuration tool
 
-      # Desk apps
-      vlc
-      # vesktop #discord alternative web thingne
-      zathura # PDF viewer
+    # Desk apps
+    vlc
+    protonvpn-gui # ProtonVPN client
+    # vesktop #discord alternative web thingne
+    zathura # PDF viewer
 
-      # Bluetooth
-      bluez-alsa # Bluetooth ALSA support
-      bluez # Bluetooth utilities
-      bluez-tools # Bluetooth tools
-      blueman # Bluetooth manager
+    # Bluetooth
+    bluez-alsa # Bluetooth ALSA support
+    bluez # Bluetooth utilities
+    bluez-tools # Bluetooth tools
+    blueman # Bluetooth manager
 
-      # Utilities
-      appimage-run # Needed for AppImage support
-      wireguard-tools # WireGuard tools
-      brightnessctl # For screen brightness control
-      bottom # another tui system usage interface
-      cliphist # Clipboard manager using rofi menu
-      cloc # Count lines of code
-      duf # Utility for viewing disk usage in terminal
-      dysk # Get information on your mounted disks tool
-      eza # Beautiful ls replacement
-      ffmpeg # Terminal video/audio editing
-      file-roller # Archive manager
-      gawk # GNU awk
-      #gh # GitHub CLI (enabled in home manager)
-      htop # Simple terminal-based system monitor
-      imv # Image viewer
-      inxi # System information tool
-      killall # Command to kill processes
-      libnotify # Notification library
-      lm_sensors # Hardware monitoring
-      lshw # Hardware information tool
-      ncdu # Disk usage analyzer
-      pavucontrol # PulseAudio volume control
-      nwg-displays # configure monitor configs via GUI
-      nwg-drawer # Rofi like menu
-      pamixer # Command-line mixer for PulseAudio
-      easyeffects # Audio effects for PipeWire
-      pciutils # PCI utilities
-      playerctl # Media player controller
-      jq # Command-line JSON processor
-      ripgrep # Fast search tool
-      socat # Multipurpose relay
-      unrar # RAR archive extractor
-      unzip # ZIP archive extractor
-      usbutils # USB utilities
-      v4l-utils # Video4Linux utilities
-      wget # Network downloader
-      wtype # typing thing
-      ytmdl # YouTube music downloader
+    # Utilities
+    appimage-run # Needed for AppImage support
+    wireguard-tools # WireGuard tools
+    brightnessctl # For screen brightness control
+    bottom # another tui system usage interface
+    cliphist # Clipboard manager using rofi menu
+    cloc # Count lines of code
+    duf # Utility for viewing disk usage in terminal
+    dysk # Get information on your mounted disks tool
+    eza # Beautiful ls replacement
+    ffmpeg # Terminal video/audio editing
+    file-roller # Archive manager
+    gawk # GNU awk
+    #gh # GitHub CLI (enabled in home manager)
+    htop # Simple terminal-based system monitor
+    imv # Image viewer
+    inxi # System information tool
+    killall # Command to kill processes
+    libnotify # Notification library
+    lm_sensors # Hardware monitoring
+    lshw # Hardware information tool
+    ncdu # Disk usage analyzer
+    pavucontrol # PulseAudio volume control
+    nwg-displays # configure monitor configs via GUI
+    nwg-drawer # Rofi like menu
+    pamixer # Command-line mixer for PulseAudio
+    easyeffects # Audio effects for PipeWire
+    pciutils # PCI utilities
+    playerctl # Media player controller
+    jq # Command-line JSON processor
+    ripgrep # Fast search tool
+    socat # Multipurpose relay
+    unrar # RAR archive extractor
+    unzip # ZIP archive extractor
+    usbutils # USB utilities
+    v4l-utils # Video4Linux utilities
+    wget # Network downloader
+    wtype # typing thing
+    yazi # Terminal file manager
+    ytmdl # YouTube music downloader
 
-      # Media
-      mpv # Media player
+    # Media
+    mpv # Media player
 
-      # Virtualization
-      libvirt # Virtualization library
-      virt-viewer # Virtual machine viewer
+    # Virtualization
+    libvirt # Virtualization library
+    virt-viewer # Virtual machine viewer
 
-      # Miscellaneous
-      greetd.tuigreet # The login manager (sometimes referred to as display manager)
-      hyprpicker # Color picker
-    ])
-    ++
-      # UNSTABLE PACKAGES!
-      (with pkgs-unstable; [
-        protonvpn-gui # ProtonVPN client
-        yazi # Terminal file manager
-      ]);
+    # Miscellaneous
+    greetd.tuigreet # The login manager (sometimes referred to as display manager)
+    hyprpicker # Color picker
+  ];
 }
