dd90b9bd55defd6054098eecaa64a97e6812b0e3
AlexAntonik <antonikavv@gmail.com>
2025-05-24 18:45:51 +0300

    some visual thing and off tlp

--- core/services.nix
+++ core/services.nix
@@ -34,26 +34,26 @@ in
     # flatpak.enable = true;
     blueman.enable = true;
 
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    # tlp = {
+    #   enable = true;
+    #   settings = {
+    #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 80;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 30;
+    #     CPU_MIN_PERF_ON_AC = 0;
+    #     CPU_MAX_PERF_ON_AC = 80;
+    #     CPU_MIN_PERF_ON_BAT = 0;
+    #     CPU_MAX_PERF_ON_BAT = 30;
 
-        #Optional helps save long term battery health
-        START_CHARGE_THRESH_BAT0 = 50; # 40 and below it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+    #     #Optional helps save long term battery health
+    #     START_CHARGE_THRESH_BAT0 = 50; # 40 and below it starts to charge
+    #     STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
 
-      };
-    };
+    #   };
+    # };
 
     journald = {
       extraConfig = ''
