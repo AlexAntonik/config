b16a5f3f44d2fd35d5b5f23ba49cb551f26b0b65
AlexAntonik <antonikavv@gmail.com>
2025-06-08 16:31:23 +0300

    idk eza stylix hypr

--- home/eza.nix
+++ home/eza.nix
@@ -2,10 +2,17 @@
   programs.eza = {
     enable = true;
     icons = "auto";
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    git = true;
+
 
     extraOptions = [
       "--group-directories-first"
       "--no-quotes"
+      "--header"
+      "--classify"
+      "--hyperlink"
       "--git-ignore"
       "--icons=always"
     ];
