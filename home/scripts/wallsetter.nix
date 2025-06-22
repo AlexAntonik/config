7099e5086d9708e94fc24201182bbfdb7381778a
AlexAntonik <antonikavv@gmail.com>
2025-06-22 19:46:25 +0300

    small refactor

--- home/scripts/wallsetter.nix
+++ home/scripts/wallsetter.nix
@@ -16,17 +16,14 @@ pkgs.writeShellScriptBin "wallsetter" ''
     notify-send -t 9000 "The wallpaper folder is expected to have more than 1 image. Exiting Wallsetter." && exit 1
   fi
 
-  # Читаем последние обои из файла
   CURRENT_WALLPAPER=""
   [ -f "$LAST_WALLPAPER_FILE" ] && CURRENT_WALLPAPER=$(cat "$LAST_WALLPAPER_FILE")
   
-  # Выбираем новые обои, отличные от текущих
   WALLPAPER=$CURRENT_WALLPAPER
   while [ "$WALLPAPER" = "$CURRENT_WALLPAPER" ]; do
     WALLPAPER=$(find ~/Pictures/Wallpapers -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
   done
 
-  # Сохраняем новые обои в файл
   echo "$WALLPAPER" > "$LAST_WALLPAPER_FILE"
 
   ${pkgs.swww}/bin/swww img "$WALLPAPER" --transition-type random --transition-step 1 --transition-fps 60
---
+++ system/time.nix
@@ -0,0 +1,19 @@
+{host, ...}:
+let
+  inherit (import ../hosts/${host}/variables.nix) timeZone defaultLocale extraLocaleSettings;
+in
+{
+  time.timeZone = "${timeZone}";
+  i18n.defaultLocale = "${defaultLocale}";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "${extraLocaleSettings}";
+    LC_IDENTIFICATION = "${extraLocaleSettings}";
+    LC_MEASUREMENT = "${extraLocaleSettings}";
+    LC_MONETARY = "${extraLocaleSettings}";
+    LC_NAME = "${extraLocaleSettings}";
+    LC_NUMERIC = "${extraLocaleSettings}";
+    LC_PAPER = "${extraLocaleSettings}";
+    LC_TELEPHONE = "${extraLocaleSettings}";
+    LC_TIME = "${extraLocaleSettings}";
+  };
+}
