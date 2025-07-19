48762689b98aff526f5213178d9b2a4d24b8068c
AlexAntonik <antonikavv@gmail.com>
2025-07-19 21:55:12 +0300

    syncthing and other stuff

---
+++ hosts/dell/syncthing.nix
@@ -0,0 +1,25 @@
+{ username, ... }:
+{
+  services.syncthing = {
+    enable = true;
+    key = "/home/${username}/.config/syncthing/keys/key.pem";
+    cert = "/home/${username}/.config/syncthing/keys/cert.pem";
+    openDefaultPorts = true;
+    extraFlags = [ "--no-default-folder" ];
+    dataDir = "/home/${username}/.local/share/syncthing";
+    configDir = "/home/${username}/.config/syncthing";
+    user = "${username}";
+    settings = {
+      devices = {
+        "asus" = {
+          id = "V3B6HRT-4PYLWXR-LUGEXW7-4VDVC3T-F7TQTSR-CSWVVGW-GSKRHQN-R3ZNUAM";
+        };
+      };
+      folders = {
+        "Notes" = {
+          path = "/home/${username}/notes";
+        };
+      };
+    };
+  };
+}
