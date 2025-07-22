2bf4a96dcc68f1d520d194a6ad5b06bfe97a352e
AlexAntonik <antonikavv@gmail.com>
2025-07-23 01:29:14 +0300

    back to pure kernel

--- hosts/swopin-nix/hardware-configuration.nix
+++ hosts/swopin-nix/hardware-configuration.nix
@@ -1,11 +1,17 @@
-{ modulesPath, pkgs, ... }:
 {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  modulesPath,
+  pkgs,
+  ...
+}: {
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
   boot.loader.grub.device = "/dev/vda";
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  #  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
   boot.tmp.cleanOnBoot = true;
-  boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+  boot.initrd.kernelModules = ["nvme"];
+  fileSystems."/" = {
+    device = "/dev/vda1";
+    fsType = "ext4";
+  };
   zramSwap.enable = true;
 }
