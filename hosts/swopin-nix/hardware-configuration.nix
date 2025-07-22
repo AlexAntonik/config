7c83890b06c83d5dba38e3215b49c284d7626a41
AlexAntonik <antonikavv@gmail.com>
2025-07-23 01:18:21 +0300

    new kernel fro srv

--- hosts/swopin-nix/hardware-configuration.nix
+++ hosts/swopin-nix/hardware-configuration.nix
@@ -1,7 +1,8 @@
-{ modulesPath, ... }:
+{ modulesPath, pkgs, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.loader.grub.device = "/dev/vda";
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.tmp.cleanOnBoot = true;
   boot.initrd.kernelModules = [ "nvme" ];
