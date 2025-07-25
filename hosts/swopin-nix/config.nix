593c047d8f61a471d320cc4ee2e94ca0d09d632c
AlexAntonik <antonikavv@gmail.com>
2025-07-26 00:26:28 +0300

    stange err fix

--- hosts/swopin-nix/config.nix
+++ hosts/swopin-nix/config.nix
@@ -14,7 +14,7 @@ in
     ./../../system/fonts.nix
     ./../../system/nh.nix # Nix helper
     ./../../system/utilities.nix # TUI utilities and tools
-    # ./../../system/ssh.nix # SSH configuration 
+    # ./../../system/ssh.nix # SSH configuration
     # ssh config below
     ./../../system/security.nix # Security settings (Polkit, RTkit, PAM)
     ./../../system/services.nix # General services (Journald, Fstrim, etc.)
@@ -95,7 +95,6 @@ in
   users.mutableUsers = true;
   users.defaultUserShell = pkgs.zsh;
 
-
   services.openssh = {
     enable = true;
     settings = {
@@ -105,13 +104,12 @@ in
     };
     ports = [ 22 ];
   };
-  users.users.${username}.openssh.authorizedKeys.keys = [
-    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUSJwUYV0e+h3Rj4+YvrsqHuolIh45KHg9Lttid1+KI alex@alex''
-    ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYa5Spl+UBXgbeh3TyFQS2Sff+2vlKT1Oiu/aS/U0SbCTOxZOzZvyvIkvR5TmYUhy2ykMIL/cDaKgGEZ3S4kEHTvmd3Th6Sq6LammjlaH+lk3ZW97hU1mEoXHVmbOmXI5gn8NP8jp01PFGmKkYiF0U5jhm3ndBpR0AaJ6EeY/zpQqNpDP6BXFcvBwKGevckJvTPVkpMavOpzhGhTx7dYJZQ/+Lh0rKxhshyv+KOKvPF6jGfeYUc8RlG33ihJVldvEdAm+h1GmAQaBMv5ShD3okqamRyQ5JncwOvMlRTc1vlT+m/JQo+aU7P/n3SeqQfiIQvGC3gA58SkTwJ0ZMESElJstiqqENr9X531CkNbMh6w3977yuXJhkeTCAmxwdD1SN8eWvHfS+5iX8DqKePyUYIfYqPrpjXBkzOlwlEnmvDhRUiwb1vbnxc+VWe7tZjb1fIWpX7oeS0X6kHbzKcCF9ccpxXoZ1v1+bJTYA89hbwGi/FvSYfySp4VcrzBzRLtvSQOlQOyico0cxY2PPNcA+T0Mz5/LdrmUiXW1ZvStA6akDqhhqknKRRRtRwfFEnaxc/gkTNkm6VaVV9vE2dKDGYtj7Ehk97cCVWIZIhNs+ghBuvWvs3t5rdjhVlgInjYohCr5PuVGGo4Segm3fzWoSVmzpN5YML2ZQdG3M5fbPdw== siarheibautrukevich@mbp-siarhei''
-  ];
 
-  
   users.users.${username} = {
+    openssh.authorizedKeys.keys = [
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUSJwUYV0e+h3Rj4+YvrsqHuolIh45KHg9Lttid1+KI alex@alex''
+      ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYa5Spl+UBXgbeh3TyFQS2Sff+2vlKT1Oiu/aS/U0SbCTOxZOzZvyvIkvR5TmYUhy2ykMIL/cDaKgGEZ3S4kEHTvmd3Th6Sq6LammjlaH+lk3ZW97hU1mEoXHVmbOmXI5gn8NP8jp01PFGmKkYiF0U5jhm3ndBpR0AaJ6EeY/zpQqNpDP6BXFcvBwKGevckJvTPVkpMavOpzhGhTx7dYJZQ/+Lh0rKxhshyv+KOKvPF6jGfeYUc8RlG33ihJVldvEdAm+h1GmAQaBMv5ShD3okqamRyQ5JncwOvMlRTc1vlT+m/JQo+aU7P/n3SeqQfiIQvGC3gA58SkTwJ0ZMESElJstiqqENr9X531CkNbMh6w3977yuXJhkeTCAmxwdD1SN8eWvHfS+5iX8DqKePyUYIfYqPrpjXBkzOlwlEnmvDhRUiwb1vbnxc+VWe7tZjb1fIWpX7oeS0X6kHbzKcCF9ccpxXoZ1v1+bJTYA89hbwGi/FvSYfySp4VcrzBzRLtvSQOlQOyico0cxY2PPNcA+T0Mz5/LdrmUiXW1ZvStA6akDqhhqknKRRRtRwfFEnaxc/gkTNkm6VaVV9vE2dKDGYtj7Ehk97cCVWIZIhNs+ghBuvWvs3t5rdjhVlgInjYohCr5PuVGGo4Segm3fzWoSVmzpN5YML2ZQdG3M5fbPdw== siarheibautrukevich@mbp-siarhei''
+    ];
     isNormalUser = true;
     description = "${gitUsername}";
     extraGroups = [
