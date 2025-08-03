280601e20c605e74bec3895668ef1e94760df564
AlexAntonik <antonikavv@gmail.com>
2025-08-03 13:05:45 +0300

    added supabase restart script and slight refactor

---
+++ system/srv/supabase-restart.nix
@@ -0,0 +1,61 @@
+{ username, pkgs, ... }:
+{
+  systemd.services.docker-compose-maintenance = {
+    description = "Docker Compose Maintenance Service";
+    serviceConfig = {
+      Type = "oneshot";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+    script = ''
+      echo "Starting Docker Compose maintenance at $(date)"
+      
+      COMPOSE_DIR="/home/${username}/projects/srv"
+      
+      if [ -d "$COMPOSE_DIR" ]; then
+        echo "Stopping Docker Compose services..."
+        cd $COMPOSE_DIR
+        ${pkgs.docker}/bin/docker compose down
+        echo "Docker Compose services stopped at $(date)"
+      else
+        echo "Error: Docker Compose file not found at $COMPOSE_DIR"
+        exit 1
+      fi
+      
+      echo "Changing file ownership..."
+      chown -R ${username}:users /home/${username}/projects/srv
+      echo "File ownership changed to ${username}:users for /home/${username}/projects/srv"
+      
+      # Forced syncthing rescan, 
+      # but this pkgs not so popular,
+      # even than briefly checked source code, off by now
+      # ${pkgs.stc-cli}/bin/stc --homedir "/home/${username}/.config/syncthing" rescan "Prod"
+
+      echo "Waiting 4 minutes..."
+      sleep 240
+      echo "Wait completed at $(date)"
+      
+      if [ -d "$COMPOSE_DIR" ]; then
+        echo "Starting Docker Compose services..."
+        cd $COMPOSE_DIR
+        ${pkgs.docker}/bin/docker compose up -d
+        echo "Docker Compose services started at $(date)"
+      else
+        echo "Error: Docker Compose file not found at $COMPOSE_DIR"
+        exit 1
+      fi
+      
+      echo "Docker Compose maintenance completed at $(date)"
+    '';
+  };
+
+
+  systemd.timers.docker-compose-maintenance = {
+    description = "Run Docker Compose Maintenance";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "03:00";
+      Persistent = true;
+    };
+  };
+}
