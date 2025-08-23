0fe3bdb3a2630c6cced1dcb7d2b583da0a968049
AlexAntonik <antonikavv@gmail.com>
2025-08-23 22:13:01 +0300

    nix overlays refactor

--- system/overlays.nix
+++
@@ -1,9 +0,0 @@
-{inputs,...}:{
-  # Simple overlay to access unstable packages through unstable.package
-  default = final: prev: {
-    unstable = import inputs.unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
-  };
-}
