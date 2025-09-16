85b1dd9863fc668db9ff3ff42f6c074f1f5c90ed
AlexAntonik <antonikavv@gmail.com>
2025-09-16 21:21:53 +0300

    remove protonvpn/fix spcial workspace gaps
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- system/desktop/network.nix
+++ system/desktop/network.nix
@@ -21,7 +21,6 @@
         "1.1.1.1" # BEST
         "8.8.8.8" # Evil
         "9.9.9.9" # Just for fun
-        "10.2.0.1" # ProtonVPN Wireguard DNS
       ];
       # dns = "none"; # Prevent NetworkManager from managing DNS
     };
@@ -31,7 +30,6 @@
       "1.1.1.1"
       "8.8.8.8"
       "9.9.9.9"
-      "10.2.0.1"
     ];
 
     timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
@@ -39,28 +37,7 @@
     # firewall.enable = false;
     firewall = {
       checkReversePath = "loose";
-      # if packets are still dropped, they will show up in dmesg
       # logReversePathDrops = true;
-      # wireguard trips rpfilter up
-      trustedInterfaces = [ "proton0" ];
-      extraCommands = ''
-        iptables -A nixos-fw -i proton0 -j ACCEPT
-        ip6tables -A nixos-fw -i proton0 -j ACCEPT
-
-        iptables -A OUTPUT -o proton0 -j ACCEPT
-        ip6tables -A OUTPUT -o proton0 -j ACCEPT
-
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-      '';
-      extraStopCommands = ''
-        iptables -D nixos-fw -i proton0 -j ACCEPT || true
-        ip6tables -D nixos-fw -i proton0 -j ACCEPT || true
-        iptables -D OUTPUT -o proton0 -j ACCEPT || true
-        ip6tables -D OUTPUT -o proton0 -j ACCEPT || true
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-      '';
     };
   };
 
