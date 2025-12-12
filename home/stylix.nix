f4788c0ffba8e45b517d5aacab9fd7724f2d8822
AlexAntonik <antonikavv@gmail.com>
2025-12-12 21:34:47 +0300

    update, some refactor
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- home/stylix.nix
+++ home/stylix.nix
@@ -14,8 +14,6 @@
     nvf.enable = false;
     starship.enable = false;
     firefox.enable = false; # for some reason causes cpu freq cap
-    #Even i don't have mako installed this fixes stylix shit
-    # mako.enable = false;
     vscode.enable = false; 
     ghostty.enable = false;
   };
--- system/docker.nix
+++ system/docker.nix
@@ -1,16 +1,7 @@
 { pkgs, ... }:
 {
-  virtualisation.docker = {
-    enable = true;
-    daemon.settings = {
-      insecure-registries = [
-        # "localhost:5000"
-        # "registry:5000"
-        # "127.0.0.1:5000"
-      ];
-      #   iptables = false;
-    };
-  };
+  virtualisation.docker.enable = true;
+  
   environment.systemPackages = with pkgs; [
     docker-compose # Allows controlling Docker from a single file
     docker-color-output # Adds color to Docker CLI output
