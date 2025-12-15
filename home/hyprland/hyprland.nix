0c1a162714ecbc1124c12ef41c066627af37b7e6
AlexAntonik <antonikavv@gmail.com>
2025-12-15 03:55:37 +0300

    pypr test
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -18,6 +18,7 @@
   home.packages = with pkgs; [
     grim
     slurp
+    unstable.pyprland
     wl-clipboard
     swappy
     hyprpicker # Color picker
@@ -112,4 +113,56 @@
     early_exit=true
     fill_shape=false
   '';
+
+  home.file.".config/hypr/pyprland.toml".text = ''
+    [pyprland]
+    plugins = [
+      "scratchpads",
+    ]
+
+    [scratchpads.term]
+    animation = "fromTop"
+    command = "ghostty --class=com.mitchellh.ghostty-dropterm"
+    class = "com.mitchellh.ghostty-dropterm"
+    unfocus = "hide"
+    lazy = true
+    size = "76% 70%"
+    position= "12% 10%"
+
+    [scratchpads.volume]
+    animation = "fromTop"
+    command = "pavucontrol"
+    class = "pavucontrol"
+    lazy = true
+    size = "40% 90%"
+
+    [scratchpads.yazi]
+    animation = "fromBottom"
+    command = "ghostty --class=com.mitchellh.ghostty-yazi -e yazi"
+    class = "com.mitchellh.ghostty-yazi"
+    lazy = true
+    position = "2% 2%"
+    size = "96% 92%"
+
+    [scratchpads.telegram-desktop]
+    animation = "fromTop"
+    pinned = false
+    lazy = true
+    unfocus = "hide"
+    # hysteresis = 10 
+    command = "telegram-desktop"
+    class = "org.telegram.desktop"
+    position = "2% 2%"
+    size = "96% 92%"
+
+    [scratchpads.thunar]
+    animation = "fromBottom"
+    command = "thunar"
+    class = "thunar"
+    unfocus = "hide"
+    lazy = true
+    position = "12% 12%"
+    size = "76% 70%"
+  '';
+
 }
