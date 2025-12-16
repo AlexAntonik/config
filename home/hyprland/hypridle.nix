fb67f2702b137f3228bf8f814ce3b198dc13a6fa
AlexAntonik <antonikavv@gmail.com>
2025-12-16 21:32:57 +0300

    update, hypridle fix
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- home/hyprland/hypridle.nix
+++ home/hyprland/hypridle.nix
@@ -15,13 +15,12 @@
         listener = [
           {
             timeout = 600;
-            on-timeout = "printf 'false' > ${env.displayStatusFile} && hyprctl dispatch dpms off && brightnessctl -d ${env.keyboardLightID} s 0 && brightnessctl -d ${env.keyboardScreenOFFLightID} s 100";
-            on-resume = "printf 'true' > ${env.displayStatusFile} && hyprctl dispatch dpms on && brightnessctl -d ${env.keyboardLightID} s 100 && brightnessctl -d ${env.keyboardScreenOFFLightID} s 0";
+            on-timeout = "hyprlock";
           }
-
           {
             timeout = 900;
-            on-timeout = "hyprlock";
+            on-timeout = "printf 'false' > ${env.displayStatusFile} && hyprctl dispatch dpms off && brightnessctl -d ${env.keyboardLightID} s 0 && brightnessctl -d ${env.keyboardScreenOFFLightID} s 100";
+            on-resume = "printf 'true' > ${env.displayStatusFile} && hyprctl dispatch dpms on && brightnessctl -d ${env.keyboardLightID} s 100 && brightnessctl -d ${env.keyboardScreenOFFLightID} s 0";
           }
         ];
       };
