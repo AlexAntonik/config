5ea6ceac75a7815a4e3b10a34373ea60433a2ee6
AlexAntonik <antonikavv@gmail.com>
2026-01-04 14:12:52 +0300

    small noctalia tweaks
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- home/hyprland/keybinds.nix
+++ home/hyprland/keybinds.nix
@@ -58,6 +58,7 @@ in
       "SUPER,Return,exec,ghostty +new-window" # Launch terminal
       "SUPER SHIFT,Return,exec,vicinae toggle"
       "SUPER ,GRAVE,exec,vicinae toggle"
+      "SUPER ,ESCAPE,exec,noctalia-shell ipc call sessionMenu toggle"
       "SUPER,W,exec,firefox" # Launch browser
       "SUPER,O,exec,obs" # Launch OBS Studio
       "SUPER,C,exec,code" # Launch VS Code
