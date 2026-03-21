2b02fe2604cdc9563d391c6ebdfed6bf15afebeb
AlexAntonik <antonikavv@gmail.com>
2026-03-21 17:22:28 +0300

    refactor
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/nix-index.nix
+++ modules/nix-index.nix
@@ -1,4 +1,5 @@
-{inputs, ...}:{
-  imports = [inputs.nix-index-database.nixosModules.default];
+{ inputs, ... }:
+{
+  imports = [ inputs.nix-index-database.nixosModules.default ];
   programs.nix-index-database.comma.enable = true;
 }
