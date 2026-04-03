b0dbadc6183ef26a505c5f6b2df7c6c42c37b622
AlexAntonik <antonikavv@gmail.com>
2026-04-03 16:15:12 +0300

    audio fix
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/media.nix
+++ modules/media.nix
@@ -8,7 +8,7 @@
     playerctl # Media player controller
     v4l-utils # Video4Linux utilities
   ];
-  
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -17,5 +17,15 @@
     audio.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
+
+    # Fix for games(dota) audio problems
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 512; # remove this line if other apps have sound problems
+      };
+    };
   };
 }
