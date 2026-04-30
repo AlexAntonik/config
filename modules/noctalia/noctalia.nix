cdcc4ac6265cb673a3cf6e5ec7caafc698151bbb
AlexAntonik <antonikavv@gmail.com>
2026-04-30 21:25:09 +0300

    fix cache
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/noctalia/noctalia.nix
+++ modules/noctalia/noctalia.nix
@@ -4,8 +4,8 @@
 }:
 {
   nix.settings = {
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [
+    substituters = [ "https://noctalia.cachix.org" ];
+    trusted-public-keys = [
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
