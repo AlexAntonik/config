fdaba70740d1bde3aaab5ad2dfdf7640d0f09c6f
AlexAntonik <antonikavv@gmail.com>
2026-07-06 21:03:45 +0300

    agenix refactr
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/secrets/agenix.nix
+++ modules/secrets/agenix.nix
@@ -5,16 +5,14 @@
   ...
 }:
 {
-
   imports = [
     inputs.agenix.nixosModules.default
   ];
   environment.sessionVariables = {
-    RULES = "${host.flakePath}/modules/secrets/secrets.nix";
+    RULES = "${host.flakePath}/modules/secrets/rules.nix";
   };
   environment.shellAliases = {
     anix = "sudo agenix -i /etc/ssh/ssh_host_ed25519_key";
   };
-
   environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
 }
