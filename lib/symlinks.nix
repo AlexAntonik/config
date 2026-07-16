304a5ab8f2d26203ba39f3cbd5327078023bc29f
AlexAntonik <antonikavv@gmail.com>
2026-07-16 14:56:58 +0300

    made symlinks user owned
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- lib/symlinks.nix
+++ lib/symlinks.nix
@@ -1,19 +1,36 @@
 { lib }:
 let
-  # mkSymlinks "vscode" { "/home/user/.config/file" = "/nix/store/.../file"; }
-  mkSymlinks = moduleName: symlinks:
-    lib.mapAttrs' (target: source:
+  # mkSymlinks "vscode" { "/home/user/.config/file" = "/path/to/file"; }
+  # Runs as the home owner so links/dirs are not root-owned.
+  mkSymlinks =
+    moduleName: symlinks:
+    lib.mapAttrs' (
+      target: source:
       let
         name = builtins.replaceStrings [ "/" "." ] [ "-" "-" ] target;
         scriptName = "${moduleName}-${name}";
+        user =
+          let
+            m = builtins.match "/home/([^/]+)/.*" target;
+          in
+          if m == null then null else builtins.head m;
       in
       lib.nameValuePair scriptName {
-        text = ''
-          mkdir -p "$(dirname ${lib.escapeShellArg target})"
-          ln -sf ${lib.escapeShellArg source} ${lib.escapeShellArg target}
-        '';
+        deps = [ "users" ];
+        text =
+          if user == null then
+            ''
+              mkdir -p "$(dirname ${lib.escapeShellArg target})"
+              ln -sfn ${lib.escapeShellArg source} ${lib.escapeShellArg target}
+            ''
+          else
+            ''
+              runuser -u ${lib.escapeShellArg user} -- mkdir -p "$(dirname ${lib.escapeShellArg target})"
+              runuser -u ${lib.escapeShellArg user} -- ln -sfn ${lib.escapeShellArg source} ${lib.escapeShellArg target}
+            '';
       }
     ) symlinks;
-in {
+in
+{
   inherit mkSymlinks;
 }
