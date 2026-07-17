da730589e2122ac270592aa52d0ee65e75957f67
AlexAntonik <antonikavv@gmail.com>
2026-07-17 15:23:14 +0300

    fix server not able to decrypt user hash
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/users/user-desktop.nix
+++ modules/users/user-desktop.nix
@@ -1,9 +1,13 @@
 { host, config, ... }:
 {
   imports = [
-    ./sectets/user-secrets.nix
+    ./../secrets/agenix.nix
     ./user-default.nix
   ];
+  age.secrets.desktop-user-pwd-hash = {
+    file = ./desktop-user-pwd-hash.age;
+    owner = host.username;
+  };
   users.users.${host.username} = {
     extraGroups = [
       "input"
--- modules/users/user-server.nix
+++ modules/users/user-server.nix
@@ -1,9 +1,13 @@
 { host, config, ... }:
 {
   imports = [
-    ./sectets/user-secrets.nix
     ./user-default.nix
+    ./../secrets/agenix.nix
   ];
+  age.secrets.server-user-pwd-hash = {
+    file = ./server-user-pwd-hash.age;
+    owner = host.username;
+  };
   users.users.${host.username} = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUSJwUYV0e+h3Rj4+YvrsqHuolIh45KHg9Lttid1+KI alex@alex"
