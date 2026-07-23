0a39bd49f6f57fe885fb1952ae8c1936053aa2d1
AlexAntonik <antonikavv@gmail.com>
2026-07-23 18:44:07 +0300

    move max_map_count to feat module
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,9 +1,5 @@
 {
   boot = {
-    # kernelPackages = pkgs.linuxPackages_zen;
-    kernel.sysctl = {
-      "vm.max_map_count" = 2147483642;
-    };
     kernelParams = [
       "usbcore.autosuspend=-1"
       "consoleblank=300" # TUI greetd display timeout
--- modules/steam.nix
+++ modules/steam.nix
@@ -1,23 +1,26 @@
-{pkgs,...}:{
+{ pkgs, ... }: {
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 2147483642; #65530 too low for some games
+  };
   programs = {
     steam = {
       enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       gamescopeSession.enable = true;
-      extraCompatPackages = [pkgs.proton-ge-bin];
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
 
     gamescope = {
       enable = true;
       capSysNice = true;
       args = [
-        "-W 2880"  # Width
-        "-H 1620"  # Height
-        "-r 120"   # Refresh rate
-        "-f"       # Fullscreen
+        "-W 2880" # Width
+        "-H 1620" # Height
+        "-r 120" # Refresh rate
+        "-f" # Fullscreen
         "--expose-wayland"
-        "--rt"     # Realtime priority
+        "--rt" # Realtime priority
       ];
     };
   };
