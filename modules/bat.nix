69d32603dd53840dd1a79013104a0bb921ed662c
AlexAntonik <antonikavv@gmail.com>
2026-09-05 00:29:21 +0300

    pager settings
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/bat.nix
+++ modules/bat.nix
@@ -5,14 +5,11 @@
   };
   programs.bat = {
     enable = true;
-    settings = {
-      pager = "less";
-      paging = "never";
-    };
+    settings.paging = "never";
     extraPackages = with pkgs.bat-extras; [
-      batman      # man pager using bat
-      batpipe     # Pipe to git/less with bat
-      batgrep     # grep alternative with bat
+      batman # man pager using bat
+      batpipe # Pipe to git/less with bat
+      batgrep # grep alternative with bat
     ];
   };
 }
