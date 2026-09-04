072034dfad10c8e50447729d667847190f4c6bdc
AlexAntonik <antonikavv@gmail.com>
2026-09-04 15:38:01 +0300

    fix ssh idle hang
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -6,6 +6,9 @@
       PasswordAuthentication = false;
       MaxAuthTries = 3;
       LoginGraceTime = 20;
+      # keep NAT state alive / reap dead clients
+      ClientAliveInterval = 30;
+      ClientAliveCountMax = 3;
     };
     ports = [ 22 ];
   };
