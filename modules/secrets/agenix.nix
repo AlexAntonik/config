4acd4c462d3a1c990959ae995037389b2330db22
AlexAntonik <antonikavv@gmail.com>
2026-09-22 17:08:32 +0300

    modules/secrets: share rules to root
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/secrets/agenix.nix
+++ modules/secrets/agenix.nix
@@ -15,4 +15,7 @@
     anix = "sudo agenix -i /etc/ssh/ssh_host_ed25519_key";
   };
   environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+  security.sudo.extraConfig = ''
+    Defaults env_keep += "RULES"
+  '';
 }
