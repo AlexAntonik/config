27594689c4b25aeafb5868d342c1f5d243395677
AlexAntonik <antonikavv@gmail.com>
2026-09-23 11:34:28 +0300

    modules/secrets: move rules to etc
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/secrets/agenix.nix
+++ modules/secrets/agenix.nix
@@ -1,20 +1,12 @@
+{ inputs, pkgs, ... }:
 {
-  inputs,
-  pkgs,
-  host,
-  ...
-}:
-{
-  imports = [
-    inputs.agenix.nixosModules.default
-  ];
-  environment.sessionVariables = {
-    RULES = "${host.flakePath}/modules/secrets/rules.nix";
-  };
-  environment.shellAliases = {
-    anix = "sudo agenix -i /etc/ssh/ssh_host_ed25519_key";
-  };
+  imports = [ inputs.agenix.nixosModules.default ];
+
+  environment.etc."agenix/rules.nix".source = ./rules.nix;
+  environment.sessionVariables.RULES = "/etc/agenix/rules.nix";
+  environment.shellAliases.anix = "sudo agenix -i /etc/ssh/ssh_host_ed25519_key";
   environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+  
   security.sudo.extraConfig = ''
     Defaults env_keep += "RULES"
   '';
