a16863f6df3f8d37437c4ab2918a01d661df1946
AlexAntonik <antonikavv@gmail.com>
2026-09-25 13:48:38 +0300

    office: change suite to collabore, add all mimes
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- modules/office.nix
+++ modules/office.nix
@@ -2,11 +2,40 @@
 {
   xdg.mime.defaultApplications = {
     "application/pdf" = "org.pwmt.zathura.desktop";
-    "application/msword" = "writer.desktop";
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "writer.desktop";
+
+    # Writer
+    "application/msword" = "com.collaboraoffice.Office.desktop";
+    "application/rtf" = "com.collaboraoffice.Office.desktop";
+    "text/rtf" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.ms-word" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.oasis.opendocument.text" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.template" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.ms-word.document.macroEnabled.12" = "com.collaboraoffice.Office.desktop";
+
+    # Calc
+    "application/vnd.ms-excel" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.oasis.opendocument.spreadsheet" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.template" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.ms-excel.sheet.macroEnabled.12" = "com.collaboraoffice.Office.desktop";
+    "text/csv" = "com.collaboraoffice.Office.desktop";
+    "application/csv" = "com.collaboraoffice.Office.desktop";
+    "text/tab-separated-values" = "com.collaboraoffice.Office.desktop";
+
+    # Impress
+    "application/vnd.ms-powerpoint" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.oasis.opendocument.presentation" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.openxmlformats-officedocument.presentationml.template" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.ms-powerpoint.presentation.macroEnabled.12" = "com.collaboraoffice.Office.desktop";
+
+    # Draw
+    "application/vnd.oasis.opendocument.graphics" = "com.collaboraoffice.Office.desktop";
+    "application/vnd.visio" = "com.collaboraoffice.Office.desktop";
   };
   environment.systemPackages = [
-    pkgs.libreoffice-qt-stable # Office suite
+    pkgs.collabora-desktop # Office suite
     pkgs.zathura # PDF viewer
   ];
 }
